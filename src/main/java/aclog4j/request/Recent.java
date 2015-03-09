ee3529b4f077be1abf5c114c2d929ff2c3419638
Getaji <t.tanaka.onion@gmail.com>
2015-03-09 13:56:34 +0900

    tweets/lookupなどで用いるRecentクラスを作成

---
+++ src/main/java/aclog4j/request/Recent.java
@@ -0,0 +1,50 @@
+package aclog4j.request;
+
+import lombok.Getter;
+import lombok.NonNull;
+
+/**
+ * javadoc here.
+ *
+ * @author Getaji
+ */
+public class Recent {
+
+    public static enum Unit {
+        DAY("d"),
+        WEEK("w"),
+        MONTH("m"),
+        YEAR("y"),
+        ;
+
+        private final String str;
+
+        private Unit(String str) {
+            this.str = str;
+        }
+
+        public String toShortString() {
+            return str;
+        }
+    }
+
+    @Getter
+    private final int period;
+
+    @Getter
+    private final Unit unit;
+
+    public Recent(int period, @NonNull Unit unit) {
+        if (period < 1) {
+            throw new IllegalArgumentException("require: 0 < period");
+        }
+
+        this.period = period;
+        this.unit = unit;
+    }
+
+    @Override
+    public String toString() {
+        return period + unit.str;
+    }
+}
